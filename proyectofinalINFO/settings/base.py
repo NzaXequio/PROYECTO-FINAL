ebcc3779cef768e5f6e3bdb1e93ad2d2333df073
mmi <mmartinibarguren@gmail.com>
2022-08-23 19:13:03 -0300

    va el base.html

--- proyectofinalINFO/settings/base.py
+++ proyectofinalINFO/settings/base.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
+#SECRET_KEY = 'django-insecure-5lswx!kgcki*8^i!5=hzc+jk_ddkqyttnj@z!5)*!mox#o6v&4'
+SECRET_KEY = 'django-insecure-2gy_fwqv4*rgy_aqx&m*qw1i3g!*j$4dpqt^c_kn1o68g&o_a!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # True cuando trabajo en local o servidor que no sea producción (lo que se sube a la nube)
--- proyectofinalINFO/urls.py
+++ proyectofinalINFO/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+#from apps.noticias_app import views
+from proyectofinalINFO import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name = "login.html") , name= "login" ),
+    path('registro/', views.registro, name='registro'),
 ]
