c4726461e13fbaf439fa0642c641b5870a2657e4
dbetania2 <daiana076@gmail.com>
2022-09-02 02:01:20 -0300

    Pagina funcionando <3

--- apps/noticias_app/models.py
+++ apps/noticias_app/models.py
@@ -8,11 +8,10 @@ class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
 
 class Noticia(models.Model):
-    autor = models.ForeignKey('usuario_app.Usuario', on_delete = models.CASCADE)
+    autor = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     titulo = models.CharField(max_length=255)
     contenido = models.TextField()
     img = models.ImageField(null=True, blank=True, upload_to='img/noticias',help_text="Seleccione una imagen para mostrar")
-    # img = models.ImageField(null=True, blank=True, upload_to='img/eventos',help_text="Seleccione una imagen para mostrar")
     creado = models.DateTimeField(default=timezone.now)
     modificado = models.DateTimeField(auto_now=True)
     publicado = models.DateTimeField(blank=True, null=True)
@@ -31,7 +30,7 @@ class Noticia(models.Model):
 
 class Comentarios(models.Model):
     noticia = models.ForeignKey('Noticia', related_name = 'comentarios', on_delete=models.CASCADE)
-    autor = models.ForeignKey('usuario_app.Usuario', on_delete = models.CASCADE)
+    autor = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     cuerpo_comentario = models.TextField()
     creado = models.DateTimeField(default=timezone.now)
     aprobado = models.BooleanField(default=False)
