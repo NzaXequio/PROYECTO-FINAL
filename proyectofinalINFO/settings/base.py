77ec63c9fb3d9adcc77be26433299dfc40af6874
nzastone@gmail.com <nzastone@gmail.com>
2022-09-03 08:47:44 -0300

    ultt

--- proyectofinalINFO/settings/base.py
+++ proyectofinalINFO/settings/base.py
@@ -22,8 +22,8 @@ from telnetlib import LOGOUT # librerías del sistema op que me permiten definir
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ #BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #busca dónde está la carpeta del proyecto dentro de cualquier compu
 
